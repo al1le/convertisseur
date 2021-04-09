47d600989c7f8ec5f3eb6c72c91117187db1c114
Alain <alain.le@bsp-sas.fr>
2021-04-09 10:36:43 +0200

    basic converter with switch

--- convertisseur/src/app/app.component.ts
+++ convertisseur/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'convertisseur';
-}
+  amountEuro:number;
+  rateEuroDollar:number;
+  amountDollar:number;
+  euroToDollar:boolean;
+  
+  ngOnInit(): void {
+    this.amountEuro = 1.1;
+    this.rateEuroDollar = 1.19;
+    this.euroToDollar = true;
+    this.amountDollar = this.amountEuro*this.rateEuroDollar;
+  }
+
+  convert() {
+    if(this.euroToDollar) {
+      this.amountDollar = this.amountEuro*this.rateEuroDollar;
+    } else {
+      this.amountDollar = this.amountEuro / this.rateEuroDollar;
+    }
+  }
+ }
--- convertisseur/src/app/app.module.ts
+++ convertisseur/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle'
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
